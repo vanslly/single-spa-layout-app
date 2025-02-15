8d370ca969abd331989d856eab87c19200065155
Llyle van Schalkwyk <llyle.vanSchalkwyk@private-flight.com>
2025-02-15 14:06:25 +1300

    Update MFEs to Angular 19

--- app-home/src/app/app.component.ts
+++ app-home/src/app/app.component.ts
@@ -4,11 +4,10 @@ import { singleSpaPropsSubject } from '../single-spa/single-spa-props';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-home-root',
-  standalone: true,
-  imports: [RouterOutlet, CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-home-root',
+    imports: [RouterOutlet, CommonModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'app-home';
